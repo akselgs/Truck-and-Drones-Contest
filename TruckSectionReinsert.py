2e147c340f8c46ad474a2bc64f6b922730613fe3
akselgs <akselslettemoen@gmail.com>
2026-04-10 13:15:00 +0200

    Made list slicing with crossover

--- TruckSectionReinsert.py
+++ TruckSectionReinsert.py
@@ -23,8 +23,8 @@ def delete_truck_section(runner, solution, section_length):
     delete_node_index = random.randint(1, len(solution)-1)
     delete_node = candidate["part1"][delete_node_index]
 
-    print("delete node")
-    print(delete_node)
+    print("section length")
+    print(section_length)
     print("delete node index")
     print(delete_node_index)
 
@@ -32,17 +32,25 @@ def delete_truck_section(runner, solution, section_length):
     print(delete_node_index-section_length)
     print("higher end")
     print(delete_node_index+section_length)
-    truck_section = truck_path_with_open_loop[(delete_node_index-section_length):(delete_node_index+section_length)]
+    truck_section = slice_with_rollover(truck_path_with_open_loop, delete_node_index-section_length, delete_node_index+section_length)
     print(truck_section)
-    leftover_section = truck_path_with_open_loop[delete_node_index+section_length:delete_node_index-section_length]
+    leftover_section = slice_with_rollover(truck_path_with_open_loop, delete_node_index+section_length, delete_node_index-section_length)
 
+    print("Truck section and leftover section")
     print(truck_section)
     print(leftover_section)
     unassigned = []
 
     return truck_section, leftover_section, unassigned
 
-    
+def slice_with_rollover(lst, start, end):
+    if start < 0:
+        sliced_list = lst[start:] + lst[:len(lst)-end]
+    elif end > len(lst):
+        sliced_list = lst[start:] + lst[:end-len(lst)]
+    else:
+        sliced_list = lst[start:end]
+    return sliced_list
 
 
 
