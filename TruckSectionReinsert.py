e07e364b5aa5dcb0a96af877fb8bf9fb712283a0
akselgs <akselslettemoen@gmail.com>
2026-04-14 14:06:07 +0200

    fnaf

--- TruckSectionReinsert.py
+++ TruckSectionReinsert.py
@@ -6,51 +6,52 @@ from Common import copy_solution
 
 def truck_section_reinsert(runner, solution):
 
-    max_section_length = len(solution["part1"])//4
+    max_section_length = len(solution["part1"])//3
     print("Max selection length:")
     print(max_section_length)
-    section_length = random.randint(1,max_section_length)
-
+    section_length = random.randint(1,max_section_length+1)
+    print("Selected section length")
+    print(section_length)
     truck_section, destroyed_candidate, unassigned = delete_truck_section(runner, solution, section_length)
     return solution
 
 def delete_truck_section(runner, solution, section_length):
     candidate = copy_solution(solution)
+    # Remove ending 0
     truck_path_with_open_loop = candidate["part1"][0:-1]
     print("open truck path:")
     print(truck_path_with_open_loop)
 
-    delete_node_index = random.randint(1, len(solution)-1)
-    delete_node = candidate["part1"][delete_node_index]
+    random_shift = random.randint(0,len(truck_path_with_open_loop))
+    print("random_shift")
+    print(random_shift)
 
-    print("section length")
-    print(section_length)
-    print("delete node index")
-    print(delete_node_index)
+    remove_section = truck_path_with_open_loop[random_shift:] + truck_path_with_open_loop[:random_shift]
+    remove_section = remove_section[:section_length]
+    print("shifted truck path")
+    print(remove_section)
+    print("leftover truck route") = remove_section[section_length:]
 
-    print("lower end")
-    print(delete_node_index-section_length)
-    print("higher end")
-    print(delete_node_index+section_length)
-    truck_section = slice_with_rollover(truck_path_with_open_loop, delete_node_index-section_length, delete_node_index+section_length)
-    print(truck_section)
-    leftover_section = slice_with_rollover(truck_path_with_open_loop, delete_node_index+section_length, delete_node_index-section_length)
 
-    print("Truck section and leftover section")
-    print(truck_section)
-    print(leftover_section)
-    unassigned = []
+    interior_nodes = []
+    orphans = []
+    exterior_nodes = []
 
-    return truck_section, leftover_section, unassigned
+    drone_id = 0
+    for i, node in enumerate(candidate["part2"]):
+        if node == -1:
+            drone_index += 1
+            continue
 
-def slice_with_rollover(lst, start, end):
-    if start < 0:
-        sliced_list = lst[start:] + lst[:len(lst)-end]
-    elif end > len(lst):
-        sliced_list = lst[start:] + lst[:end-len(lst)]
-    else:
-        sliced_list = lst[start:end]
-    return sliced_list
+        launch_node = candidate["part1"][candidate["part3"][i]]
+        receiver_node = candidate["part1"][candidate["part4"][i]]
+
+        if (launch_node in remove_section) and (receiver_node in remove_section):
+            interior_nodes.append(node)
+        elif (launch_node in remove_section) or (receiver_node in remove_section):
+            orphans.append(node)
+        else:
+            exterior_nodes.append(node)
 
 
 
