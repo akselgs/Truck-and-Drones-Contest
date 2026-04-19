594898d9cddd97d3d7aab8e96053a8794f12ee5a
akselgs <akselslettemoen@gmail.com>
2026-04-19 17:07:22 +0200

    improved performance

--- SimAnnMultipleOps.py
+++ SimAnnMultipleOps.py
@@ -7,21 +7,25 @@ from TruckSectionReinsert import truck_section_reinsert
 from FlattenSection import flatten_section
 
 def sim_ann_multiple_ops(runner, iterations):
+    # Calibration split:
     split = iterations // 100
-    rand = random.randint(0,100)
-    rand = rand/100
     delta_w = []
     t_f = 0.1
-    op_1_threshold = 30
-    op_2_threshold = 30
 
-    scores = [1.0, 1.0, 1.0]  # one per operator
+    ###Weights of operator. Should match nr of operators
+    # scores = [1.0, 1.0, 1.0] #Scores if we want adjustable adaptive weights
     weights = [1.0, 1.0, 1.0]
-    new_best_reward = 3
-    improvement_reward = 2
-    sa_accept_reward = 1
-    decay = 0.5
+
+    ###Rewards for updating scores
+    # new_best_reward = 3
+    # improvement_reward = 2
+    # sa_accept_reward = 1
+    # decay = 0.5
+
+
+    #--Start
     result = runner.run()
+
     if result["feasible"]:
         best_solution = copy_solution(runner.solution)
         best_objective = result["objective"]
@@ -32,78 +36,77 @@ def sim_ann_multiple_ops(runner, iterations):
         print("ERROR- Initial result is not feasible")
 
 
+    #-- Calibrate temperature
     for w in range(split):
         rand = random.randint(0,100)
         rand = rand/100
         
-        if (w % 100 == 0):
+        if (w % 250 == 0):
             print()
             print("Iteration", w)
+
 
         op = random.choices([0, 1, 2], weights=weights)[0]
         if op == 0:
             candidate_solution, candidate_objective = one_reinsert(runner, incumbent_solution)
+        
         elif op == 1:
             candidate_solution, candidate_objective = truck_section_reinsert(runner, incumbent_solution)
+
         else:
             candidate_solution, candidate_objective = flatten_section(runner, incumbent_solution)
 
-        if not candidate_solution:
-            if w % 100 == 0: 
-                print("No insertion positions found, continuing to next iteration from calibration-split.")
-            continue
+
+
         candidate_feasible = runner.is_solution_feasible(candidate_solution)
 
         delta_e = candidate_objective - incumbent_objective
         if candidate_feasible and (delta_e < 0):
             incumbent_solution = copy_solution(candidate_solution)
             incumbent_objective = candidate_objective
-            scores[op] += improvement_reward
+            # scores[op] += improvement_reward
+            
             if incumbent_objective < best_objective:
                 best_solution =copy_solution(incumbent_solution)
                 print()
                 print("New best solution found")
                 print(best_solution)
                 print(best_objective)
-                scores[op] += new_best_reward
+                # scores[op] += new_best_reward
 
         elif candidate_feasible:
             if rand < 0.8: 
                 incumbent_solution = copy_solution(candidate_solution)
                 incumbent_objective = candidate_objective
-                scores[op] += sa_accept_reward
+                # scores[op] += sa_accept_reward
             delta_w.append(delta_e)
-        if w % 100 == 0:
-            weights[op] = (1 - decay) * weights[op] + decay * scores[op]
-            scores = [1.0, 1.0, 1.0]  # reset scores    
+
+        ###Update weights based on scores. Reset sometimes..
+        # weights[op] = (1 - decay) * weights[op] + decay * scores[op]
+        # if w % 100 == 0:
+        #     scores = [1.0, 1.0, 1.0]
+
     if len(delta_w) == 0:
         print("Error, delta_W is empty, likely indicating too small sample size.")
-        delta_w.append(2)
+
 
     delta_avg = sum(delta_w)/len(delta_w)
     t_0 = (-1 * delta_avg)/(math.log(0.8))
     alpha = (t_f / t_0) ** (1/(iterations - split))
     t = t_0
     
+    # Main iteration loop:
     for i in range(iterations - split):
         rand = random.randint(0,100)
         rand = rand/100
 
-        
-
-        early_stop_counter += 1
-        if early_stop_counter > 10000:
-            return best_solution
         if (i % 500 == 0):
             print()
             print("Iteration", i + split)
-            print("Early stop counter:", early_stop_counter)
             print("Weights")
             print(weights)
-        #print("incumb")
-        #print(incumbent_runner.solution)
+
         op = random.choices([0, 1, 2], weights=weights)[0]
-        #rand_op = 1
         if op == 0:
             candidate_solution, candidate_objective = one_reinsert(runner, incumbent_solution)
         elif op == 1:
@@ -111,28 +114,36 @@ def sim_ann_multiple_ops(runner, iterations):
         else:
             candidate_solution, candidate_objective = flatten_section(runner, incumbent_solution)
 
+        #If no insertions are found using one-reinsert, it will return none.
         if not candidate_solution:
             if i % 100 == 0: 
                 print("No insertion positions found, continuing to next iteration from calibration-split.")
             continue
+
+        #Check feasibility
         candidate_feasible = runner.is_solution_feasible(candidate_solution)
-        #print("cand")
-        #print(candidate_runner.solution)
 
         delta_e = candidate_objective - incumbent_objective
-        try: 
-            p = math.exp(-1*delta_e/t) 
-        except:
-            print("ERROR: Program crashed at - p = math.exp(-1*delta_e/t)")
-            print("alpha", alpha)
-            print("delta_e", delta_e)
-            print("t", t)
-            print("t_0", t_0)
-            return "Error"
+
+        if delta_e < 0:
+            p = 1
+        else:
+        #This sometimes fails, we include some print statement for debugging in case it does.
+            try: 
+                p = math.exp(-1*delta_e/t) 
+            except:
+                print("ERROR: Program crashed at - p = math.exp(-1*delta_e/t)")
+                print("alpha", alpha)
+                print("delta_e", delta_e)
+                print("t", t)
+                print("t_0", t_0)
+                return "Error"
+        
+
         if candidate_feasible and (delta_e < 0):
             incumbent_solution = copy_solution(candidate_solution)
             incumbent_objective = candidate_objective
-            scores[op] += improvement_reward
+            # scores[op] += improvement_reward
             
             if incumbent_objective < best_objective:
                 best_solution = copy_solution(incumbent_solution)
@@ -142,27 +153,18 @@ def sim_ann_multiple_ops(runner, iterations):
                 print()
                 print(best_solution)
                 print(best_objective)
-                early_stop_counter = 0
-                scores[op] += new_best_reward
+                # scores[op] += new_best_reward
                 
         elif candidate_feasible and (rand <  p):
             if delta_e != 0:
                 incumbent_solution = copy_solution(candidate_solution)
                 incumbent_objective = candidate_objective
-                #print()
-                #print("=======================")
-                if i % 100 == 0:
-                    print("Worse solution explored, operation:", op)
-                #print(incumbent_solution)
-                #print(incumbent_objective)
-                #print("Delta E", delta_e)
-                #print("=======================")
-                scores[op] += sa_accept_reward
+                # scores[op] += sa_accept_reward
         t = alpha * t
 
-        if i%100 == 0:
-            weights[op] = (1 - decay) * weights[op] + decay * scores[op]
-            scores = [1.0, 1.0, 1.0]  # reset scores
+        # weights[op] = (1 - decay) * weights[op] + decay * scores[op]
+        # if i%100 == 0:
+        #     scores = [1.0, 1.0, 1.0]
     
     print("final weights:")
     print(weights)
--- TruckSectionReinsert.py
+++ TruckSectionReinsert.py
@@ -11,8 +11,8 @@ def truck_section_reinsert(runner, solution):
     max_section_length = len(solution["part1"])//3
     section_length = random.randint(1,max_section_length+1)
     
-    remove_section, leftover_section, interior_nodes, exterior_nodes, orphans, shift, depot_sorties = delete_truck_section(runner, solution, section_length)
-    best_candidate, best_cost = best_section_insert(remove_section, leftover_section, interior_nodes,exterior_nodes, orphans, shift, runner, solution, section_length, depot_sorties)
+    remove_section, leftover_section, interior_nodes, exterior_nodes, orphans, depot_sorties = delete_truck_section(runner, solution, section_length)
+    best_candidate, best_cost = best_section_insert(remove_section, leftover_section, interior_nodes,exterior_nodes, orphans, runner, solution, depot_sorties)
 
 
     return best_candidate, best_cost
@@ -76,34 +76,27 @@ def delete_truck_section(runner, solution, section_length):
         else:
             exterior_nodes.append((node, sender_node, receiver_node, drone_index))
 
-    return(remove_section, leftover_section, interior_nodes, exterior_nodes, orphans, shift, depot_sorties)
+    return(remove_section, leftover_section, interior_nodes, exterior_nodes, orphans, depot_sorties)
 
 
-def best_section_insert(remove_section, leftover_section, interior_nodes, exterior_nodes, orphans, shift, runner, solution, section_length, depot_sorties):
-
-
-    best_candidate = copy_solution(solution)
-    best_cost, arr, dep, feas = runner.calculate_total_waiting_time(solution)
+def best_section_insert(remove_section, leftover_section, interior_nodes, exterior_nodes, orphans, runner, solution, depot_sorties):
+    best_cost = float('inf')
+    best_candidate = None
 
     #For insertion positions
     for i in range(len(leftover_section)+1):
         #For orientation
         for o in range(2):
-            if o == 0:
-                truck_candidate = leftover_section.copy()
-                insert = remove_section.copy()
-                truck_candidate = leftover_section[:i] + insert + leftover_section[i:]
-            else:
-                truck_candidate = leftover_section.copy()
-                insert = remove_section.copy()
+            truck_candidate = leftover_section.copy()
+            insert = remove_section.copy()
+            if o == 1:
                 insert.reverse()
-                truck_candidate = leftover_section[:i] + insert + leftover_section[i:]
+            truck_candidate = leftover_section[:i] + insert + leftover_section[i:]
 
-
+            # We shift back so 0 is at the start again, and add the 0 at the end back
             shift = truck_candidate.index(0)
             truck_candidate = truck_candidate[shift:] + truck_candidate[:shift]
             truck_candidate.append(0)
-
 
             p2d1, p3d1, p4d1 = [], [], []
             p2d2, p3d2, p4d2 = [], [], []
@@ -133,55 +126,6 @@ def best_section_insert(remove_section, leftover_section, interior_nodes, exteri
                     p3d2.append(s_index + 1)
                     p4d2.append(r_index + 1)
 
-            # for n, s, r, d in (exterior_nodes):
-            #     s_index = truck_candidate.index(s)
-            #     r_index = truck_candidate.index(r)
-            #     if d == 0:
-
-            #         pos = bisect.bisect_left(p3d1, s_index)
-            #         p2d1.insert(pos,n)
-            #         p3d1.insert(pos,s_index+1)
-            #         p4d1.insert(pos,r_index+1)
-            #     else:
-            #         pos = bisect.bisect_left(p3d2, s_index)
-            #         p2d2.insert(pos,n)
-            #         p3d2.insert(pos,s_index+1)
-            #         p4d2.insert(pos,r_index+1)
-
-            # helper_interior_insert = []
-            # for n, s, r, d in (interior_nodes):
-            #     if o == 1:
-            #         s, r = r, s
-            #     s_index = truck_candidate.index(s)
-            #     r_index = truck_candidate.index(r)
-
-            #     if d == 0:
-            #         pos = bisect.bisect_left(p3d1, s_index)
-            #         p2d1.insert(pos,n)
-            #         p3d1.insert(pos,s_index+1)
-            #         p4d1.insert(pos,r_index+1)
-            #     else:
-            #         pos = bisect.bisect_left(p3d2, s_index)
-            #         p2d2.insert(pos,n)
-            #         p3d2.insert(pos,s_index+1)
-            #         p4d2.insert(pos,r_index+1)
-
-            
-            # for n, s, r, d in (depot_sorties):
-            #     if n in orphans:
-            #         continue
-            #     s_index = truck_candidate.index(s)
-            #     if d == 0:
-            #         pos = bisect.bisect_left(p3d1, s_index)
-            #         p2d1.insert(pos,n)
-            #         p3d1.insert(pos,s_index+1)
-            #         p4d1.insert(pos,len(truck_candidate))
-            #     else:
-            #         pos = bisect.bisect_left(p3d2, s_index)
-            #         p2d2.insert(pos,n)
-            #         p3d2.insert(pos,s_index+1)
-            #         p4d2.insert(pos,len(truck_candidate))
-
             candidate = {
                 "part1" : truck_candidate,
                 "part2" : p2d1 + [-1] + p2d2,
@@ -189,47 +133,52 @@ def best_section_insert(remove_section, leftover_section, interior_nodes, exteri
                 "part4" : p4d1 + [-1] + p4d2,
             }
 
+            # Very costly if we check orphans for all insert positions. Instead we select ideal insert position first, then add orphans later.
 
             obj, arr, dep, feas = runner.calculate_total_waiting_time(candidate)
 
+            if (obj < best_cost) and feas:
+                best_candidate = copy_solution(candidate)
+                best_cost = obj.copy()
 
-            if feas:
-                if len(orphans) > 0:
-                    for node in orphans:
-                        try:
-                            candidate_tuples = best_single_insert_random_select(runner, candidate, node)
-                        except:
-                            print("exterior_nodes")
-                            print(exterior_nodes)
-                            print("interior nodes")
-                            print(interior_nodes)
-                            print("Truck candidate")
-                            print(truck_candidate)
-                            print("orientation")
-                            print(o)
-                            print("Oprhans")
-                            print(orphans)
-                            print("Shift")
-                            print(shift)
-                            print("remove section")
-                            print(remove_section)
-                            print("leftovers")
-                            print(leftover_section)
-                            print("Old candidate")
-                            print(solution)
-                            print("Candidate")
-                            print(candidate)
-                            print(node)
-                        if len(candidate_tuples) == 0:
-                            return None, None
-                        else:
-                            candidate, objective = random_select_candidate(candidate_tuples)
+    if best_candidate:    
+        if len(orphans) > 0:
+            for node in orphans:
+                try:
+                    candidate_tuples = best_single_insert_random_select(runner, best_candidate, node)
+                except:
+                    print("exterior_nodes")
+                    print(exterior_nodes)
+                    print("interior nodes")
+                    print(interior_nodes)
+                    print("Truck candidate")
+                    print(truck_candidate)
+                    print("orientation")
+                    print(o)
+                    print("Oprhans")
+                    print(orphans)
+                    print("Shift")
+                    print(shift)
+                    print("remove section")
+                    print(remove_section)
+                    print("leftovers")
+                    print(leftover_section)
+                    print("Old candidate")
+                    print(solution)
+                    print("Candidate")
+                    print(candidate)
+                    print(node)
+
+                if len(candidate_tuples) == 0:
+                    return None, None
                 else:
-                    objective = obj
-
+                    candidate, objective = random_select_candidate(candidate_tuples)
                 if objective < best_cost:
-                    best_cost = objective
                     best_candidate = candidate
+                    best_cost = objective
+    
+    else:
+        return None, None
 
     return best_candidate, best_cost
 
